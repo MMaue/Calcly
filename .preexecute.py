78a48ed3040d773ff6a734002990dfa4b09a827c
Miguel Mauer <73600075+MMaue@users.noreply.github.com>
2023-07-14 20:39:10 +0200

    Update .preexecute.py
    there should be a simple vector environment

--- .preexecute.py
+++ .preexecute.py
@@ -57,6 +57,26 @@ def contour(f, fill=True,
 	plt.colorbar()
 	plt.show()
 
+#TODO vector env
+'''
+https://matplotlib.org/stable/gallery/mplot3d/quiver3d.html
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+soa = np.array([[0, 0, 1, 1, -2, 0], [0, 0, 2, 1, 1, 0],
+                [0, 0, 3, 2, 1, 0], [0, 0, 4, 0.5, 0.7, 0]])
+
+X, Y, Z, U, V, W = zip(*soa)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.quiver(X, Y, Z, U, V, W)
+ax.set_xlim([-1, 0.5])
+ax.set_ylim([-1, 1.5])
+ax.set_zlim([-1, 8])
+plt.show()
+'''
+
 # plt.style.use('ggplot')
 # plt.style.use('dark_background')
 # plt.rcParams['figure.figsize'] = 15, 10
